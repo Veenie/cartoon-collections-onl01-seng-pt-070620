ae55ac329a28a3d5bffe6b4ce0d04bf421c40c8a
Raveen K Sharma <raveen08@gmail.com>
2020-07-14 21:25:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   # Your code here
-  planeteer_calls.map |call| call.capitilize
+  planeteer_calls.map |call| call.capitilize + "!"
 end
 
 def long_planeteer_calls# code an argument here
