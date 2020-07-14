59fd5d34f3a62991fd0b86d11b2b4210e53a3f19
Raveen K Sharma <raveen08@gmail.com>
2020-07-14 21:29:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|call| call.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
